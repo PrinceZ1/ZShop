cc16b5ef6bd0bff73d829da336b6f88444182075
TNguyen <nguyen.tt1703@gmail.com>
2025-07-09 05:56:37 +0700

    Initial commit

---
+++ src/main/java/com/princez1/ZShop/ZShopApplication.java
@@ -0,0 +1,13 @@
+package com.princez1.ZShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZShopApplication.class, args);
+	}
+
+}
